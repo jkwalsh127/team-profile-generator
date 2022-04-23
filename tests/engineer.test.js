eb775941d991665460ad77224031934f909ccae9
Jake Walsh <jkwalsh127@gmail.com>
2022-04-22 22:29:41 -0700

    complete engineer tests

---
+++ tests/engineer.test.js
@@ -0,0 +1,64 @@
+const Engineer = require('../lib/engineer');
+
+describe('Engineer', () => {
+    describe('Initialization', () => {
+        //Positive test
+        it("should create an object with a name, id, email, and github if give valid inputs", () => {
+            //Arrange
+            const name = 'Sam';
+            const id = 4;
+            const email = 'sam@gmail.com';
+            const github = 'samwise';
+
+            //Act
+            const obj = new Engineer(name, id, email, github);
+
+            //Assert
+            expect(obj.name).toEqual(name);
+            expect(obj.id).toEqual(id);
+            expect(obj.email).toEqual(email);
+            expect(obj.github).toEqual(github);
+        });
+        //Exception test
+        it("should throw an error if not provided a valid 'name' value", () => {
+            //Arrange
+            const cb = () => new Engineer(1, 4, 'sam@gmail.com', 'samwise');
+            const err = new Error(
+                "Expected parameter 'name' to be a non empty string"
+            );
+
+            //Assert
+            expect(cb).toThrowError(err);
+        });
+        it("should throw an error if not provided a valid 'id' value", () => {
+            //Arrange
+            const cb = () => new Engineer('Sam', 'asf', 'sam@gmail.com', 'samwise');
+            const err = new Error(
+                "Expected parameter 'id' to be a non-negative number"
+            );
+
+            //Assert
+            expect(cb).toThrowError(err);
+        });
+        it("should throw an error if not provided a valid 'email' value", () => {
+            //Arrange
+            const cb = () => new Engineer('Sam', 4, 3, 'samwise');
+            const err = new Error(
+                "Expected parameter 'email' to be a non empty string in email format"
+            );
+
+            //Assert
+            expect(cb).toThrowError(err);
+        });
+        it("should throw an error if not provided a valid 'github' value", () => {
+            //Arrange
+            const cb = () => new Engineer('Sam', 4, 'sam@gmail.com', 1);
+            const err = new Error(
+                "Expected parameter 'github' to be a non empty string"
+            );
+
+            //Assert
+            expect(cb).toThrowError(err);
+        });
+    });
+});
